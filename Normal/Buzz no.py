7bf03ee7b9b09e20eeefa462b8c5354038bdada3
Rupanjan Dutta <rupanjandutta06@gmail.com>
2026-02-16 12:52:19 +0530

    Fix Buzz number condition to check for 7

--- Normal/Buzz no.py
+++ Normal/Buzz no.py
@@ -1,6 +1,6 @@
 #WAP to check whether a no is Buzz or not
 n=int(input("Enter the No = "))
-if n%7==0 and n%10==0:
+if n%7==0 and n%10==7:
     print(n,"is a Buzz Number")
 else:
     print(n,"is not a Buzz Number")
